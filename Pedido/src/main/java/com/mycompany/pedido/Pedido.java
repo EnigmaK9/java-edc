4050ec9d992176168028cbce72af47d9460eca7a
Ignacio Padilla <enigmak9@protonmail.com>
2024-05-31 11:20:03 -0600

    added 04 sentencias pedido program

---
+++ Pedido/src/main/java/com/mycompany/pedido/Pedido.java
@@ -0,0 +1,23 @@
+package com.mycompany.pedido;
+
+/**
+ *
+ * Author: enigma
+ */
+public class Pedido {
+
+    public static void main(String[] args) {
+        double costoArticulo = 30.99;
+        boolean listoEnviar = false;
+
+        if (costoArticulo > 24.00) {
+            System.out.println("¡El valor del artículo es alto!");
+        }
+
+        if (listoEnviar) {
+            System.out.println("Enviado");
+        } else {
+            System.out.println("Pedido no listo");
+        }
+    }
+}
