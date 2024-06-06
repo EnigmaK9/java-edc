5f07e5accefbe78baeb6aaf45915fb60d4e4d797
EnigmaK9 <enigmak9@protonmail.com>
2024-06-06 13:12:26 -0600

    added for program

---
+++ pairNumbersWithFOr/pairNumbersWithFor.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class pairNumbersWithFor {
+    public static void main(String[] args) {
+        int n = 20;
+        for(int i = 2; i <= n; i = i + 2){
+            System.out.println(i);
+        }
+        System.out.println("Try programiz.pro");
+    }
+}
