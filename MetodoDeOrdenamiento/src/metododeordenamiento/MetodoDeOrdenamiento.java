b4d544b00068b8c3d1285bea7f5e2b70b74c9b7c
enigmak9 <enigmak9@protonmail.com>
2024-06-12 12:10:27 -0600

    added code done in june 12 2024

--- MetodoDeOrdenamiento/src/metododeordenamiento/MetodoDeOrdenamiento.java
+++ MetodoDeOrdenamiento/src/metododeordenamiento/MetodoDeOrdenamiento.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package metododeordenamiento;
 
 /**
  *
- * @author Enigm
+ * @author Carlos I. Padilla Herrera
  */
 public class MetodoDeOrdenamiento {
 
