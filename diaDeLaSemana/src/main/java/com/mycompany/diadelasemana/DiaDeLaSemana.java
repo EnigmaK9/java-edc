d0c0bb231a5e5b68990bb042350ec748cfe18e09
enigmak9 <enigmak9@protonmail.com>
2024-06-12 19:44:33 -0600

    modified diaDeLaSemana code

--- diaDeLaSemana/src/main/java/com/mycompany/diadelasemana/DiaDeLaSemana.java
+++ diaDeLaSemana/src/main/java/com/mycompany/diadelasemana/DiaDeLaSemana.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 package com.mycompany.diadelasemana;
 
 import java.util.Scanner;
 
 /**
- * Clase principal para determinar el día de la semana basado en la entrada del usuario.
+ * Clase principal para determinar el dia de la semana basado en la entrada del usuario.
  * Autor: enigma
  */
 public class DiaDeLaSemana {
@@ -15,16 +12,16 @@ public class DiaDeLaSemana {
         // Crear un objeto Scanner
         Scanner semana = new Scanner(System.in);
         
-        // Crear una variable para almacenar el número del día
+        // Crear una variable para almacenar el numero del dia
         int dia;
         
         // Solicitar la entrada del usuario
-        System.out.println("Introduce un número del 1 al 7 para conocer el día de la semana:");
+        System.out.println("Introduce un numero del 1 al 7 para conocer el dia de la semana:");
         
         // Leer la entrada del usuario
         dia = semana.nextInt();
         
-        // Estructura switch-case para determinar el día de la semana
+        // Estructura switch-case para determinar el dia de la semana
         switch (dia) {
             case 1:
                 System.out.println("Lunes");
@@ -33,7 +30,7 @@ public class DiaDeLaSemana {
                 System.out.println("Martes");
                 break;
             case 3:
-                System.out.println("Miércoles");
+                System.out.println("Miercoles");
                 break;
             case 4:
                 System.out.println("Jueves");
@@ -42,14 +39,14 @@ public class DiaDeLaSemana {
                 System.out.println("Viernes");
                 break;
             case 6:
-                System.out.println("Sábado");
+                System.out.println("Sabado");
                 break;
             case 7:
                 System.out.println("Domingo");
                 break;
             default:
-                // Manejar entrada no válida
-                System.out.println("Número no válido.");
+                // Manejar entrada no valida
+                System.out.println("Numero no valido.");
                 break;
         }
         
